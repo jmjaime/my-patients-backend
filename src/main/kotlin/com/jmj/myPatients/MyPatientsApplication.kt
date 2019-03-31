5eac50a47e6f52bb24771bbea44824a8d05e013f
Juan Manuel Jaime <jmj155@gmail.com>
2019-03-31 16:03:34 -0300

    Initial commit

---
+++ src/main/kotlin/com/jmj/myPatients/MyPatientsApplication.kt
@@ -0,0 +1,11 @@
+package com.jmj.myPatients
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyPatientsApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyPatientsApplication>(*args)
+}
